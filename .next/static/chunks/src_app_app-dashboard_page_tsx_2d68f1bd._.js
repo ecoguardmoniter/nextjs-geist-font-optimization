6717f7e031fa00bfc34c2f9a163963dc8b41be1c
ecoguardmoniter <satypolo0909@gmail.com>
2025-07-30 11:03:32 +0530

    Add .next/static/chunks/src_app_app-dashboard_page_tsx_2d68f1bd._.js 1753853612147

---
+++ .next/static/chunks/src_app_app-dashboard_page_tsx_2d68f1bd._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_75236a41._.js",
+  "static/chunks/src_d63d104b._.js"
+],
+    source: "dynamic"
+});
